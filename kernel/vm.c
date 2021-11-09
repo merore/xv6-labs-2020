e68601d5c2dc2a1bb3b957e6695d9b0e922e3ee7
merore <merore256@outlook.com>
2021-11-09 15:49:44 +0800

    print pagetable

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprintrec(pagetable_t pagetable, int level)
+{
+	for(int i=0;i<512;i++)
+	{
+		pte_t pte=pagetable[i];
+		if(pte & PTE_V)
+		{
+			for(int n=0;n<level;n++)
+				printf(".. ");
+			printf("..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+			if(level<2)
+				vmprintrec((pagetable_t)PTE2PA(pte), level+1);
+		}
+	}
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+	printf("page table %p\n",pagetable);
+	vmprintrec(pagetable, 0);
+}
