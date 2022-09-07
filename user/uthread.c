083a4a792eb1d7e175796b36cf50cf8cc35dc0f3
merore <merore256@outlook.com>
2022-09-07 21:49:59 +0800

    modified:   user/uthread.c 	modified:   user/uthread_switch.S 	modified:   notxv6/ph.c 	modified:   notxv6/barrier.c

--- user/uthread.c
+++ user/uthread.c
@@ -9,13 +9,17 @@
 
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
+#define CONTEXT_SIZE 14
 
+#define RA 0
+#define SP 1
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+	uint64		 context[CONTEXT_SIZE];
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
@@ -63,6 +67,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+		thread_switch((uint64)t->context, (uint64)next_thread->context);
   } else
     next_thread = 0;
 }
@@ -77,6 +82,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+	t->context[RA] = (uint64)func;
+	t->context[SP] = (uint64)((void *)t->context+STACK_SIZE);
 }
 
 void 
